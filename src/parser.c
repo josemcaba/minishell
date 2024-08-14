b59b168130fd6d74d2fc6fc80b7ed706ec2454c9
Jose M. Caballero (dynabook) <josemcaba@gmail.com>
2024-08-14 13:10:14 +0200

    Probando

--- src/parser.c
+++ src/parser.c
@@ -17,31 +17,31 @@
 #define MAX_CMD_LEN 1024
 #define MAX_NUM_PARAMS 64
 
-void	parse_command(char* cmd, char** params) {
-	int paramIndex = 0;
-	int cmdIndex = 0;
-	int inQuotes = 0;
-	int backslash = 0;
+void	parse_command(char *cmd, char **params) {
+int	paramIndex = 0;
+int	cmdIndex = 0;
+int	inQuotes = 0;
+int	backslash = 0;
 
-	for(int i = 0; i < MAX_CMD_LEN; i++) {
-		if(cmd[i] == '\\') {
-			backslash = 1;
-			continue;
-		}
-		if(backslash) {
-			backslash = 0;
-		} else if(cmd[i] == '\"') {
-			inQuotes = !inQuotes;
-		} else if((cmd[i] == '|' && !inQuotes) || cmd[i] == '\0') {
-			char* param = malloc(i - cmdIndex + 1);
-			strncpy(param, &cmd[cmdIndex], i - cmdIndex);
-			param[i - cmdIndex] = '\0';
-			params[paramIndex++] = param;
-			if(cmd[i] == '\0') {
-				break;
-			}
-			cmdIndex = i + 1;
-		}
+for(int i = 0; i < MAX_CMD_LEN; i++) {
+	if(cmd[i] == '\\') {
+		backslash = 1;
+		continue;
 	}
-	params[paramIndex] = NULL;
+	if(backslash) {
+		backslash = 0;
+	} else if(cmd[i] == '\"') {
+		inQuotes = !inQuotes;
+	} else if((cmd[i] == '|' && !inQuotes) || cmd[i] == '\0') {
+		char* param = malloc(i - cmdIndex + 1);
+		strncpy(param, &cmd[cmdIndex], i - cmdIndex);
+		param[i - cmdIndex] = '\0';
+		params[paramIndex++] = param;
+		if(cmd[i] == '\0') {
+			break;
+		}
+		cmdIndex = i + 1;
+	}
+}
+params[paramIndex] = NULL;
 }
